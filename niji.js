4be83ed58ec48dbe716ed699a9cb58f8e0a852d3
Sunbrigh <152992184+Sunbrigh@users.noreply.github.com>
2023-12-05 21:48:23 +0700

    niji.js

--- niji.js
+++ niji.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Aaaaa, apakah amet memek?";
+  question.innerHTML = "apakah baiyu ngeslot?";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
