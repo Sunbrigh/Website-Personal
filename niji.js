1cd3799ea041c548b2be2c21caa0131d963edd5e
Sunbrigh <152992184+Sunbrigh@users.noreply.github.com>
2023-12-07 17:43:27 +0700

    Update niji.js

--- niji.js
+++ niji.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "私も😁?";
+  question.innerHTML = "愛してます😁";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
